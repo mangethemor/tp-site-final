906639081630307eef54d8031d83a2a8262fce42
Pot 2Fleur <pot2fleur@gmail.com>
2022-11-14 20:26:23 +0100

    Add table for Post/Reply/Comment

--- database/migrations/2022_11_14_185736_create_posts_table.php
+++ database/migrations/2022_11_14_185736_create_posts_table.php
@@ -14,7 +14,10 @@ return new class extends Migration
     public function up()
     {
         Schema::create('posts', function (Blueprint $table) {
-            $table->id();
+            $table->bigIncrements('id');
+            $table->unsignedInteger('user_id');
+            $table->string('title');
+            $table->text('body');
             $table->timestamps();
         });
     }
--- database/migrations/2022_11_14_190104_create_replies_table.php
+++ database/migrations/2022_11_14_190104_create_replies_table.php
@@ -12,9 +12,13 @@ return new class extends Migration
      * @return void
      */
     public function up()
+    public function up()
     {
         Schema::create('replies', function (Blueprint $table) {
-            $table->id();
+            $table->bigIncrements('id');
+            $table->unsignedInteger('user_id');
+            $table->string('title');
+            $table->text('body');
             $table->timestamps();
         });
     }
