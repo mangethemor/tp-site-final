f82e6be61f0537522d90d832dd748c1583f75845
Pot 2Fleur <pot2fleur@gmail.com>
2022-11-15 10:44:45 +0100

    Fixed Authentification

--- database/migrations/2022_11_14_190104_create_replies_table.php
+++ database/migrations/2022_11_14_190104_create_replies_table.php
@@ -12,7 +12,6 @@ return new class extends Migration
      * @return void
      */
     public function up()
-    public function up()
     {
         Schema::create('replies', function (Blueprint $table) {
             $table->bigIncrements('id');
--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,8 @@
 <?php
 
+use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
-
-use App\Http\Controllers\CommentController;
-use App\Http\Controllers\HomeController;
-use App\Http\Controllers\PostController;
-use App\Http\Controllers\ReplyController;
+use Inertia\Inertia;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,32 +16,16 @@ use App\Http\Controllers\ReplyController;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return Inertia::render('Welcome', [
+        'canLogin' => Route::has('login'),
+        'canRegister' => Route::has('register'),
+        'laravelVersion' => Application::VERSION,
+        'phpVersion' => PHP_VERSION,
+    ]);
 });
 
 Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
+    return Inertia::render('Dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
 require __DIR__.'/auth.php';
-
-// group the following routes by auth middleware - you have to be signed-in to proceeed
-Route::group(['middleware' => 'auth'], function() {
-	// Dashboard
-	Route::get('/home', [HomeController::class, 'index'])->name('home');
-
-	// Posts resourcefull controllers routes
-	Route::resource('posts', PostController::class);
-
-	// Comments routes
-	Route::group(['prefix' => '/comments', 'as' => 'comments.'], function() {
-        // store comment route
-		Route::post('/{post}', [CommentController::class, 'store'])->name('store');
-	});
-
-	// Replies routes
-	Route::group(['prefix' => '/replies', 'as' => 'replies.'], function() {
-        // store reply route
-		Route::post('/{comment}', [ReplyController::class, 'store'])->name('store');
-	});
-});
