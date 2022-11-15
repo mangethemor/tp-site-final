7ca8da4deb6bc2c1c2abaab99d679856e904a09a
Pot 2Fleur <pot2fleur@gmail.com>
2022-11-15 09:42:31 +0100

    Reset to last commit

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\CommentController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\ReplyController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -17,6 +22,35 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+<<<<<<< HEAD
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';
+
+// group the following routes by auth middleware - you have to be signed-in to proceeed
+Route::group(['middleware' => 'auth'], function() {
+	// Dashboard
+	Route::get('/home', [HomeController::class, 'index'])->name('home');
+
+	// Posts resourcefull controllers routes
+	Route::resource('posts', PostController::class);
+
+	// Comments routes
+	Route::group(['prefix' => '/comments', 'as' => 'comments.'], function() {
+        // store comment route
+		Route::post('/{post}', [CommentController::class, 'store'])->name('store');
+	});
+
+	// Replies routes
+	Route::group(['prefix' => '/replies', 'as' => 'replies.'], function() {
+        // store reply route
+		Route::post('/{comment}', [ReplyController::class, 'store'])->name('store');
+	});
+});
+=======
 Route::get('/tarifs', function () {
     return view('tarifs');
 });
+>>>>>>> c7037cd1f656b6690e887607d1bc0279800521c4
